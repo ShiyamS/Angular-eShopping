47b683cb80348443a65b23cac7f15e9fbe9214ad
Shiyam <shiyams315@gmail.com>
2023-09-07 09:38:42 +0530

    ngClass Directive

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -3,11 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notification',
   template:`
-  <div class="alert alert-success" [hidden]="displayNotification">
+  <div class="alert alert-success" [ngClass]="{fadeOut:displayNotification}">
     This website uses cookies to provide better user experience.
     <div class="close"><button class="btn" (click)="closeNotification()">X</button></div>
   </div>`,
-  styles: [`.notification-div{margin:10px 0px; padding: 10px 20px; background-color: #fad780;text-align:center}`,`p{font-size:14px}`,`.close{float:right; margin-top: -13px}`]
+  styles: [`.notification-div{margin:10px 0px; padding: 10px 20px; background-color: #fad780;text-align:center;}`,
+  `p{font-size:14px;}`,
+  `.close{float:right; margin-top: -13px;}`,
+  `.fadeOut{visibility:hidden; opacity:0; transition: visibility 0s 2s opacity 2s linear}`,
+  ]
 })
 export class NotificationComponent {
 

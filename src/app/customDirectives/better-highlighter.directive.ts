863c4d4825f3efc04120775139f65e4d5cc2d6f0
Shiyam <shiyams315@gmail.com>
2023-09-13 12:27:18 +0530

    @HostBinding -> Directive

--- src/app/customDirectives/better-highlighter.directive.ts
+++ src/app/customDirectives/better-highlighter.directive.ts
@@ -1,26 +1,35 @@
-import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlighter]'
 })
-export class BetterHighlighterDirective {
+export class BetterHighlighterDirective implements OnInit {
 
   constructor(private element: ElementRef, private renderer: Renderer2) {
   }
 
   //  Host Binding to its property in th ehtml element
 
-  @HostBinding('style.background') background: string = "transparent"
+  @Input() defaultColor: string = 'transparent';
+  @Input('appBetterHighlighter') setColor: string = 'pink';
+
+  @HostBinding('style.background') background: string = this.defaultColor;
   @HostBinding('style.border') border: string = "none"
+
+  @Input() title: string = 'This is a Title of a dive'
+
+  ngOnInit(): void {
+    this.background = this.defaultColor
+  }
 
   //  Listens the DOM event and trigger an event
   @HostListener('mouseenter') mouseEnter() {
-    this.background = 'pink';
+    this.background = this.setColor;
     this.border = 'purple 2px solid';
   }
 
   @HostListener('mouseleave') mouseLeave() {
-    this.background = 'transparent';
+    this.background = this.defaultColor;
     this.border = 'none';
   }
 }

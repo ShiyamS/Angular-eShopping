400f8bc37e4f2e3a40c7bc9efe977ecf0be9ce22
Shiyam <shiyams315@gmail.com>
2023-09-18 16:06:59 +0530

    Creating observable and subscribing it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { DemoComponent } from './demo/demo.component';
 import { UserService } from './services/user.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -26,7 +28,26 @@ export class AppComponent implements OnInit {
 
   // }
 
+  myObservable = new Observable((observer) => {
+    console.log("Observer started")
+    setTimeout(() => { observer.next(1) }, 1000)
+    setTimeout(() => { observer.next(2) }, 2000)
+    setTimeout(() => { observer.next(3) }, 3000)
+    setTimeout(() => { observer.next(4) }, 4000)
+    setTimeout(() => { observer.next(5) }, 5000)
+    // observer.next(1)
+    // observer.next(2)
+    // observer.next(3)
+    // observer.next(4)
+    // observer.next(5)
+  })
+
   ngOnInit(): void {
+
+    this.myObservable.subscribe((value) => {
+      console.log(value);
+
+    })
     // this.users = this.userService.users
   }
   // calculateAge() {
